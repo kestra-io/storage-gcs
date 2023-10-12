b32eb5672161f4a4472c8076d8fd8db779476c8a
Martin GUIBERT <mguibert@kestra.io>
2023-10-12 10:58:28 +0200

    chore: update to use new secretinterface with tenant

--- src/test/java/io/kestra/storage/gcs/GcsStorageTest.java
+++ src/test/java/io/kestra/storage/gcs/GcsStorageTest.java
@@ -28,8 +28,9 @@ class GcsStorageTest {
     @Inject
     StorageInterface storageInterface;
 
-    private URI putFile(URL resource, String path) throws Exception {
+    private URI putFile(String tenantID, URL resource, String path) throws Exception {
         return storageInterface.put(
+            tenantID,
             new URI(path),
             new FileInputStream(Objects.requireNonNull(resource).getFile())
         );
@@ -38,39 +39,42 @@ class GcsStorageTest {
     @Test
     void get() throws Exception {
         String prefix = IdUtils.create();
+        String tenantId = IdUtils.create();
 
         URL resource = GcsStorageTest.class.getClassLoader().getResource("application.yml");
         String content = CharStreams.toString(new InputStreamReader(new FileInputStream(Objects.requireNonNull(resource).getFile())));
 
-        this.putFile(resource, "/" + prefix + "/storage/get.yml");
+        this.putFile(tenantId, resource, "/" + prefix + "/storage/get.yml");
 
         URI item = new URI("/" + prefix + "/storage/get.yml");
-        InputStream get = storageInterface.get(item);
+        InputStream get = storageInterface.get(tenantId,item);
         assertThat(CharStreams.toString(new InputStreamReader(get)), is(content));
-        assertTrue(storageInterface.exists(item));
-        assertThat(storageInterface.size(item), is((long) content.length()));
-        assertThat(storageInterface.lastModifiedTime(item), notNullValue());
+        assertTrue(storageInterface.exists(tenantId,item));
+        assertThat(storageInterface.size(tenantId, item), is((long) content.length()));
+        assertThat(storageInterface.lastModifiedTime(tenantId, item), notNullValue());
 
-        InputStream getScheme = storageInterface.get(new URI("kestra:///" + prefix + "/storage/get.yml"));
+        InputStream getScheme = storageInterface.get(tenantId, new URI("kestra:///" + prefix + "/storage/get.yml"));
         assertThat(CharStreams.toString(new InputStreamReader(getScheme)), is(content));
     }
 
     @Test
     void missing() {
         String prefix = IdUtils.create();
+        String tenantId = IdUtils.create();
 
         assertThrows(FileNotFoundException.class, () -> {
-            storageInterface.get(new URI("/" + prefix + "/storage/missing.yml"));
+            storageInterface.get(tenantId, new URI("/" + prefix + "/storage/missing.yml"));
         });
     }
 
     @Test
     void put() throws Exception {
         String prefix = IdUtils.create();
+        String tenantId = IdUtils.create();
 
         URL resource = GcsStorageTest.class.getClassLoader().getResource("application.yml");
-        URI put = this.putFile(resource, "/" + prefix + "/storage/put.yml");
-        InputStream get = storageInterface.get(new URI("/" + prefix + "/storage/put.yml"));
+        URI put = this.putFile(tenantId, resource, "/" + prefix + "/storage/put.yml");
+        InputStream get = storageInterface.get(tenantId, new URI("/" + prefix + "/storage/put.yml"));
 
         assertThat(put.toString(), is(new URI("kestra:///" + prefix + "/storage/put.yml").toString()));
         assertThat(
@@ -78,26 +82,27 @@ class GcsStorageTest {
             is(CharStreams.toString(new InputStreamReader(new FileInputStream(Objects.requireNonNull(resource).getFile()))))
         );
 
-        assertThat(storageInterface.size(new URI("/" + prefix + "/storage/put.yml")), is(76L));
+        assertThat(storageInterface.size(tenantId, new URI("/" + prefix + "/storage/put.yml")), is(76L));
 
         assertThrows(FileNotFoundException.class, () -> {
-            assertThat(storageInterface.size(new URI("/" + prefix + "/storage/muissing.yml")), is(76L));
+            assertThat(storageInterface.size(tenantId, new URI("/" + prefix + "/storage/muissing.yml")), is(76L));
         });
 
-        boolean delete = storageInterface.delete(put);
+        boolean delete = storageInterface.delete(tenantId, put);
         assertThat(delete, is(true));
 
-        delete = storageInterface.delete(put);
+        delete = storageInterface.delete(tenantId, put);
         assertThat(delete, is(false));
 
         assertThrows(FileNotFoundException.class, () -> {
-            storageInterface.get(new URI("/" + prefix + "/storage/put.yml"));
+            storageInterface.get(tenantId, new URI("/" + prefix + "/storage/put.yml"));
         });
     }
 
     @Test
     void deleteByPrefix() throws Exception {
         String prefix = IdUtils.create();
+        String tenantId = IdUtils.create();
 
         URL resource = GcsStorageTest.class.getClassLoader().getResource("application.yml");
 
@@ -107,20 +112,20 @@ class GcsStorageTest {
             "/" + prefix + "/storage/level1/level2/1.yml"
         );
 
-        path.forEach(throwConsumer(s -> this.putFile(resource, s)));
+        path.forEach(throwConsumer(s -> this.putFile(tenantId, resource, s)));
 
-        List<URI> deleted = storageInterface.deleteByPrefix(new URI("/" + prefix + "/storage/"));
+        List<URI> deleted = storageInterface.deleteByPrefix(tenantId, new URI("/" + prefix + "/storage/"));
 
         assertThat(deleted, containsInAnyOrder(path.stream().map(s -> URI.create("kestra://" + s)).toArray()));
 
         assertThrows(FileNotFoundException.class, () -> {
-            storageInterface.get(new URI("/" + prefix + "/storage/"));
+            storageInterface.get(tenantId, new URI("/" + prefix + "/storage/"));
         });
 
         path
             .forEach(s -> {
                 assertThrows(FileNotFoundException.class, () -> {
-                    storageInterface.get(new URI(s));
+                    storageInterface.get(tenantId, new URI(s));
                 });
             });
     }
@@ -128,8 +133,9 @@ class GcsStorageTest {
     @Test
     void deleteByPrefixNoResult() throws Exception {
         String prefix = IdUtils.create();
+        String tenantId = IdUtils.create();
 
-        List<URI> deleted = storageInterface.deleteByPrefix(new URI("/" + prefix + "/storage/"));
+        List<URI> deleted = storageInterface.deleteByPrefix(tenantId, new URI("/" + prefix + "/storage/"));
         assertThat(deleted.size(), is(0));
     }
 }
