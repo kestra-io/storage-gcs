6e507ffbc21e1651b74f2290b43ca69d5a10f161
YannC <37600690+Skraye@users.noreply.github.com>
2026-04-28 12:12:31 +0200

    feat: use PluginProperty instead of JsonProperty (#192)

--- src/main/java/io/kestra/storage/gcs/GcsConfig.java
+++ src/main/java/io/kestra/storage/gcs/GcsConfig.java
@@ -1,20 +1,31 @@
 package io.kestra.storage.gcs;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import io.kestra.core.models.annotations.PluginProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public interface GcsConfig {
 
-    @JsonProperty
+    @Schema(
+        title = "The GCS bucket where to store internal objects."
+    )
+    @PluginProperty
     @NotNull
     @NotBlank
     String getBucket();
 
-    @JsonProperty
+    @Schema(
+        title = "The GCS service account key, as a JSON string.",
+        description = "If not provided, the default credentials will be used."
+    )
+    @PluginProperty
     String getServiceAccount();
 
-    @JsonProperty
+    @Schema(
+        title = "The GCP project ID."
+    )
+    @PluginProperty
     String getProjectId();
 }
